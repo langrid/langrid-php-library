6a32751acff03dc47b19d9fb6e288ad0ece493f2
Masayuki Otani <m-otani@i.kyoto-u.ac.jp>
2014-02-17 12:04:12 +0900

    文字コード問題用の修正
    Modifiied task.sh to avoid the language code problem.

--- test/task.sh
+++ test/task.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-#cd ../test/
+cd ../test/
+export LANG=ja_JP.UTF-8
 phpunit --testdox-text ./tmp/testdox.txt AllTests.php > ./tmp/stdout.txt
 cat ./tmp/stdout.txt ./tmp/testdox.txt | mail langrid-ot@ai.soc.i.kyoto-u.ac.jp -s "Language Resource test with PHPUnit"
