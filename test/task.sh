dcca2dd6936d94679a9c965fc029aefb4c7f724a
Masayuki Otani <m-otani@i.kyoto-u.ac.jp>
2014-04-07 15:40:11 +0900

    改行コードCRLFが紛れ込んでmailxで転送できない問題に対処＠sa-bisu tesuto

--- test/task.sh
+++ test/task.sh
@@ -2,4 +2,5 @@
 cd ../test/
 export LANG=ja_JP.UTF-8
 phpunit --testdox-text ./tmp/testdox.txt AllTests.php > ./tmp/stdout.txt
+nkf -Lu -w --overwrite ./tmp/stdout.txt
 cat ./tmp/stdout.txt ./tmp/testdox.txt | mail langrid-ot@ai.soc.i.kyoto-u.ac.jp -s "Language Resource test with PHPUnit"
